fe0cdbe7535d00ef69276bedc54c4bef84348c5e
Anthony Chablov <achablov@myseneca.ca>
2024-07-10 09:32:39 -0400

    feat: intro animations

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useRef } from "react";
+import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import ParagraphText from "../Text/ParagraphText";
 
@@ -10,25 +11,45 @@ const Intro = () => {
   const mainLayerRef = useRef(null);
   const paragraphRef = useRef(null);
 
+  useGSAP(
+    () => {
+      let tl = gsap.timeline({});
+      tl.fromTo(
+        firstLayerRef.current,
+        { opacity: 0 },
+        { opacity: 1, duration: 1 }
+      )
+        .to(secondLayerRef.current, { y: "100%", duration: 1 }, "+=0.5")
+        .to(mainLayerRef.current, { y: "100%", duration: 1 }, "+=0.5")
+        .to(
+          [firstLayerRef.current, secondLayerRef.current, mainLayerRef.current],
+          { opacity: 0, duration: 1 },
+          "+=0.5"
+        );
+    },
+    { scope: introRef }
+  );
+
   return (
     <div ref={introRef} className="relative">
       <div
         ref={firstLayerRef}
-        className="h-screen bg-black absolute top-0 left-0 "
+        className="h-screen w-full bg-black absolute top-0 left-0 z-0"
       ></div>
       <div
         ref={secondLayerRef}
-        className="h-screen bg-white absolute top-0 left-0"
+        className="h-screen w-full bg-white absolute top-0 left-0 z-10"
       ></div>
       <div
         ref={mainLayerRef}
-        className="h-screen flex bg-black justify-center place-items-center absolute top-0 left-0"
+        className="h-screen w-full flex bg-black justify-center 
+          items-center absolute top-0 left-0 z-20"
       >
         <ParagraphText
           ref={paragraphRef}
           text="Melriver"
-          mode="4xl"
-          className="font-medium"
+          mode="7xl"
+          className="font-medium text-white"
         />
       </div>
     </div>
--- src/components/Text/ParagraphText.tsx
+++ src/components/Text/ParagraphText.tsx
@@ -2,7 +2,19 @@ import React from "react";
 import { cn } from "@/lib/utils";
 
 interface ParagraphTextProps {
-  mode?: "sm" | "md" | "lg" | "xl" | "2xl" | "3xl" | "4xl";
+  mode?:
+    | "sm"
+    | "md"
+    | "lg"
+    | "xl"
+    | "2xl"
+    | "3xl"
+    | "4xl"
+    | "5xl"
+    | "6xl"
+    | "7xl"
+    | "8xl"
+    | "9xl";
   text?: string;
   className?: string;
   children?: React.ReactNode;
@@ -35,6 +47,21 @@ const ParagraphText = React.forwardRef<
       break;
     case "4xl":
       modeClass = "text-4xl";
+      break;
+    case "5xl":
+      modeClass = "text-5xl";
+      break;
+    case "6xl":
+      modeClass = "text-6xl";
+      break;
+    case "7xl":
+      modeClass = "text-7xl";
+      break;
+    case "8xl":
+      modeClass = "text-8xl";
+      break;
+    case "9xl":
+      modeClass = "text-9xl";
       break;
     default:
       modeClass = "text-base"; // Default to medium size if mode is not recognized
